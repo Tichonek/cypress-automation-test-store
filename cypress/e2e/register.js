9d26cd69471bbe8ccb46091068abf4b53ab35c14
Filip <meksykanin.32@gmail.com>
2024-08-19 19:59:06 +0200

    add function to fill form with data

--- cypress/e2e/register.js
+++ cypress/e2e/register.js
@@ -2,6 +2,7 @@
 
 import HomePage from "../pageObject/HomePage";
 import AccountLoginPage from "../pageObject/AccountLoginPage";
+import CreateAccountForm from "../pageObject/CreateAccountForm";
 
 describe("Register new account", () => {
 
@@ -24,6 +25,15 @@ describe("Register new account", () => {
 
         // assertions
         cy.url().should("include", "rt=account/create")
+        cy.CheckFormVisibility()
+
+        //CreateAccountForm.getFirstNameField().type(data.firstName)
+        CreateAccountForm.fillPersonalDetailsForm({
+            firstName: data.personalDeatils.firstName, 
+            lastName: data.personalDeatils.lastName
+        })
+
+        CreateAccountForm.getFirstNameField().should("have.value", data.personalDeatils.firstName)
 
     })
 })
