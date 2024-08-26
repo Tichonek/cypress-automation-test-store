330795fff1e7bdc5947806702d777a86b52c8a54
Filip <meksykanin.32@gmail.com>
2024-08-26 18:13:12 +0200

    add selectors for login form

--- cypress/pageObject/AccountLoginPage.js
+++ cypress/pageObject/AccountLoginPage.js
@@ -2,6 +2,24 @@ class AccountLoginPage {
     getContinueButton() {
         return cy.get("button[title='Continue']")
     }
+
+    getLoginInput() {
+        return cy.get("#loginFrm_loginname")
+    }
+
+    getPasswordInput() {
+        return cy.get("#loginFrm_password")
+    }
+
+    getLoginButton() {
+        return cy.get("button[title='Login']")
+    }
+
+    fillLoginForm({login, password} = {}) {
+        if(login) this.getLoginInput().type(login)
+        if(password) this.getPasswordInput().type(password)
+
+    }
 }
 
 export default new AccountLoginPage()
